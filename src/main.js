7be0ccebf798a3f2a3c33f054336626385e77edd
武岳 <guardwu@126.com>
2020-12-27 17:15:47 +0800

    feat: 初始化项目

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import '@/custom-component' // 注册自定义组件
 
+import '@/styles/reset.css'
+import '@/styles/animate.css'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI, { size: 'small' })
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
